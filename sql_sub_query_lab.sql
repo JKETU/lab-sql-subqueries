24f749388f191d91c387fe6552823200479b6012
Joel Ketu <joelketu96@gmail.com>
2024-09-01 23:24:30 +0200

    Add files via upload

--- sql_sub_query_lab.sql
+++ sql_sub_query_lab.sql
@@ -5,4 +5,14 @@ JOIN film f ON i.film_id = f.film_id
 WHERE f.title = 'Hunchback Impossible';
 
 -- 2. List all films whose length is longer than the average length of all the films in the Sakila database.
+SELECT title, length
+FROM film
+WHERE length > (SELECT AVG(length) FROM film);
+
+-- 3. Use a subquery to display all actors who appear in the film "Alone Trip".
+
+SELECT a.first_name, a.last_name
+FROM actor a
+WHERE a.actor_id IN (SELECT fa.actor_id FROM film_actor fa JOIN film f ON fa.film_id = f.film_id WHERE f.title = 'Alone Trip'
+    );
 
